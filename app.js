68c7bba4c754004935c136f5b8eb57d4869ff1dd
FelisbinoJose <246760.jose@unibave.net>
2026-03-08 20:59:44 -0300

    ✨ New Feature: adiciona função de gerar números aleatórios

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ function sortear() {
     const de = parseInt(document.getElementById('de').value);
     const ate = parseInt(document.getElementById('ate').value);
 }
+
+function obterNumeroAleatorio(max, min) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
