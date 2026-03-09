4a4ed6c9585325558fae444ce2ec8cd3f062c477
FelisbinoJose <246760.jose@unibave.net>
2026-03-08 21:03:18 -0300

    ✨ New Feature: adiciona lista de números sorteados

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ function sortear() {
     const quantidade = parseInt(document.getElementById('quantidade').value);
     const de = parseInt(document.getElementById('de').value);
     const ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+
+    for (let i = 0; i < quantidade; i++) {
+        let numero = obterNumeroAleatorio(ate, de);
+        sorteados.push(numero);    
+    }
+
 }
 
 function obterNumeroAleatorio(max, min) {
