1d657c38317e81f01252d8f981dfa71a3c1b4f0a
JoseFelisbino <lu31660@gmail.com>
2026-04-06 22:24:32 -0300

    feat: adiciona validação de números reptidos

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ function sortear() {
 
     for (let i = 0; i < quantidade; i++) {
         let numero = obterNumeroAleatorio(ate, de);
+
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+
         sorteados.push(numero);    
     }
 
